ec8e9a8bbcc6cadf023616b71af3e396cfe2069b
leeeha <jxlhe46@seoultech.ac.kr>
2022-07-14 21:09:31 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component {
+  render(){ // 클래스 안에 속한 메소드는 function 키워드 생략 가능  
+    return (
+      // 컴포넌트는 반드시 하나의 최상위 태그만 사용해야 함. 
+      <header>
+        <h1>WEB</h1>
+        World Wide Web!
       </header>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+class TOC extends Component { // Table Of Content (목차) 
+  render(){
+    return(
+      // jsx라는 문법 덕분에 js 파일에 html 태그를 넣을 수 있음. 
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render(){
+    return(
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+         <Subject></Subject>
+         <TOC></TOC>
+         <Content></Content>
+      </div>
+    );
+  }
+}
+
+export default App; 
