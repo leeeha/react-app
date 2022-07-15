5970ea3093b96b2ad68fdfe1c977b88efe3731b9
leeeha <jxlhe46@seoultech.ac.kr>
2022-07-15 20:07:15 +0900

    props의 데이터를 state에서 가져오기

--- src/App.js
+++ src/App.js
@@ -5,12 +5,34 @@ import Content from "./components/Content"
 import './App.css';
 
 class App extends Component {
+  // render() 보다 먼저 실행되어 초기화를 담당하는 생성자 
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}, 
+      // 속성이 가진 데이터가 여러 개일 때는 배열 사용
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interaction'}
+      ]
+    }
+  }
+
   render(){
     return (
       <div className="App">
-         <Subject title="WEB" sub="world wide web!"></Subject>
-         <Subject title="React" sub="For UI"></Subject>
-         <TOC></TOC>
+         <Subject
+            // props의 데이터를 state에서 가져옴. 
+            title={this.state.subject.title}
+            sub={this.state.subject.sub}>
+          </Subject>
+
+          <TOC 
+            // contents에 담긴 배열 정보를 TOC 컴포넌트에 주입하기 
+            data={this.state.contents}>
+          </TOC>
+
          <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 
-class TOC extends Component { 
+class TOC extends Component {
     render(){
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while(i < data.length){
+            // 여러 항목을 자동으로 생성할 때는, 각 항목이 key 값을 가져야 한다. 
+            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a>
+            </li>)
+            i++;
+        }
+
         return(
-        <nav>
-            <ul>
-                <li><a href="1.html">HTML</a></li>
-                <li><a href="2.html">CSS</a></li>
-                <li><a href="3.html">JavaScript</a></li>
-            </ul>
-        </nav>
+            <nav>
+                <ul>
+                    {lists}
+                </ul>
+            </nav>
         );
     }
 }
